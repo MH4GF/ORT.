bca4e278781c43591fdf1f07b23b1483b07b3e53
MH4GF <rixi436@gmail.com>
2018-03-28 01:32:30 +0900

    updateメソッド追加、投稿の編集ができるようになった

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,4 +15,15 @@ class PostsController < ApplicationController
     @post.save
     redirect_to("/posts/index")
   end
+
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.content = params[:content]
+    @post.save
+    redirect_to("/posts/index")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'posts/index' => "posts#index"
-  get 'posts/new' => "posts#new"
-  get 'posts/:id' => "posts#show"
+  get "posts/index" => "posts#index"
+  get "posts/new" => "posts#new"
+  get "posts/:id" => "posts#show"
   post "posts/create" => "posts#create"
+  get "posts/:id/edit" => "posts#edit"
+  post "posts/:id/update" => "posts#update"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'posts#index'
+  root "posts#index"
 end
