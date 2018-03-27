e69103d3ffa78bfadb3e4533b3a6da0c85272137
MH4GF <rixi436@gmail.com>
2018-03-28 01:10:56 +0900

    createメソッド追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
   end
 
   def show
@@ -8,5 +8,11 @@ class PostsController < ApplicationController
   end
 
   def new
+  end
+
+  def create
+    @post = Post.new(content: params[:content])
+    @post.save
+    redirect_to("/posts/index")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'posts/index' => "posts#index"
   get 'posts/new' => "posts#new"
   get 'posts/:id' => "posts#show"
-
+  post "posts/create" => "posts#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index'
 end
