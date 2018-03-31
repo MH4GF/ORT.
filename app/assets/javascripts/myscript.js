7132d640035915a61e855ed3bfb3ca8772ff54a0
MH4GF <rixi436@gmail.com>
2018-03-31 23:43:55 +0900

    サブメニュー機能完成

--- app/assets/javascripts/myscript.js
+++ app/assets/javascripts/myscript.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
-  // $('.post-submenus-btn').click(function(){
-  //   $('.post-submenus').show();
-  // });
-  $("p").css("color", "blue");
+  $('.post-submenus-btn').click(function(){
+    $(this).prev('.post-submenus').toggleClass('open');
+  });
+
+  $(document).on('click touchend', function(event) {
+    if (!$(event.target).closest('.posts-index-item').length) {
+      $('.post-submenus').removeClass('open');
+    }
+  });
 });
