97824b1c9c5fc35ccb650bc69e83d0044cdc5580
MH4GF <rixi436@gmail.com>
2018-04-07 15:44:20 +0900

    タイマー機能こと始め

---
+++ app/assets/javascripts/timer.js
@@ -0,0 +1,75 @@
+(function(){
+  'use strict';
+
+  var timer = document.getElementById('timer');
+  var min;
+  var set = document.getElementById('set');
+  var reset = document.getElementById('reset');
+  var start = document.getElementById('start');
+
+  var startTime;
+  var timeLeft;
+  var timeToCountDown = 0;
+  var timerId;
+  var isRunning = false;
+
+  function updateTimer(t){
+    var d = new Date(t);
+    var m = d.getMinutes();
+    var s = d.getSeconds();
+    var timerstring;
+    m = ('0' + m).slice(-2);
+    s = ('0' + s).slice(-2);
+    timerstring =  m + ':' + s;
+    timer.textContent = timerstring;
+    document.title = timerstring;
+  }
+
+  function countDown(){
+    timerId = setTimeout(function(){
+      timeLeft = timeToCountDown - (Date.now() - startTime);
+      if (timeLeft < 0){
+        isRunning = false;
+        start.textContent = 'Start';
+        clearTimeout(timerId);
+        timeLeft = 0;
+        timeToCountDown = 0;
+        updateTimer(timeLeft);
+        return;
+      }
+      updateTimer(timeLeft);
+      countDown();
+    }, 10);
+  }
+
+  start.addEventListener('click', function(){
+    if (isRunning === false){
+      isRunning = true;
+      start.textContent = 'Stop';
+      startTime = Date.now();
+      countDown()
+    } else {
+      isRunning = false;
+      start.textContent = 'Start';
+      timeToCountDown = timeLeft;
+      clearTimeout(timerId);
+    }
+  });
+
+  set.addEventListener('click', function(){
+    if (isRunning === true){
+      return;
+    }
+    min = parseInt(document.getElementById('min').value);
+    timeToCountDown = min * 60 * 1000;
+    if (timeToCountDown >= 60 * 60 * 1000){
+      timeToCountDown = 0;
+    }
+    updateTimer(timeToCountDown);
+  });
+
+  reset.addEventListener('click', function(){
+    timeToCountDown = 0;
+    updateTimer(timeToCountDown);
+  });
+})();
