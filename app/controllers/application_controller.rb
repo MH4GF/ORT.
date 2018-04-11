369fe87f963f07718a5bf331e8f471261928ef93
MH4GF <rixi436@gmail.com>
2018-04-11 19:02:29 +0900

    ユーザー情報表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :tags
+  before_action :tags, :sum_running_time, except: :about
 
   def tags
     @all_tags = ActsAsTaggableOn::Tag.all
+  end
+
+  def sum_running_time
+    @sum = 0
+    @user_posts = current_user.posts
+
+    @user_posts.each do |u_post|
+      @sum += u_post.running_time.to_i
+    end
+
+    @hour = @sum / 60
+    @min = @sum % 60
+
   end
 end
