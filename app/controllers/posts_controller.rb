21835e7bc03bff0f8201c96a4237f01cc31fae04
MH4GF <rixi436@gmail.com>
2018-04-12 13:53:08 +0900

    Twitter投稿成功

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,8 +22,8 @@ class PostsController < ApplicationController
     client = Twitter::REST::Client.new do |config|
       config.consumer_key         = ENV['TWITTER_CONSUMER_KEY']
       config.consumer_secret      = ENV['TWITTER_CONSUMER_SECRET']
-      config.access_token         = ENV['TWITTER_ACCESS_TOKEN']
-      config.access_token_secret  = ENV['TWITTER_ACCESS_TOKEN_SECRET']
+      config.access_token         = current_user.token
+      config.access_token_secret  = current_user.secret
     end
     # Twitter投稿
     client.update(tweet_contents)
