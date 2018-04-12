9fb7afbe0eb3749914d02643c7ba215ca3fac918
MH4GF <rixi436@gmail.com>
2018-04-12 18:31:45 +0900

    Twitterログインの不具合を解消

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -27,6 +27,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # def after_omniauth_failure_path_for(scope)
   #   super(scope)
   # end
+
   def facebook
     callback_from :facebook
   end
@@ -41,13 +42,15 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     provider = provider.to_s
 
     @user = User.find_for_oauth(request.env['omniauth.auth'])
+    @user = User.find_for_oauth(request.env['omniauth.auth'])
 
     if @user.persisted?
       flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
       sign_in_and_redirect @user, event: :authentication
     else
       session["devise.#{provider}_data"] = request.env['omniauth.auth']
-      redirect_to new_user_registration_url
+      redirect_to("/")
     end
   end
+
 end
