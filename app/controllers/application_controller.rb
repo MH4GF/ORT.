e4bc6a0211a81d89d7149e518740cfb146fe8b93
MH4GF <rixi436@gmail.com>
2018-05-31 21:07:28 +0900

    タグ毎の累計時間を表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,15 @@ class ApplicationController < ActionController::Base
   # CSRF対策
   protect_from_forgery with: :exception
 
-  before_action :tags, :sum_running_time
+  before_action :posts,:tags
 
   def posts
+    if current_user === nil
+      return
+    end
+
     @posts = current_user.posts.order(created_at: :desc)
+    sum_running_time(@posts)
   end
 
   def tags
@@ -17,7 +22,7 @@ class ApplicationController < ActionController::Base
   end
 
   # 累計時間を計算
-  def sum_running_time
+  def sum_running_time(some_posts)
     if current_user === nil
       return
     end
@@ -25,8 +30,8 @@ class ApplicationController < ActionController::Base
     @sum = 0
 
     # postsに入っている投稿の配列から、running_timeカラムに入っている時間を@sumに足していく
-    posts.each do |u_post|
-      @sum += u_post.running_time.to_i
+    some_posts.each do |post|
+      @sum += post.running_time.to_i
     end
 
     @hour = @sum / 60
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -8,6 +8,7 @@ end
 def show
   @id = ActsAsTaggableOn::Tag.find_by(id: params[:id])
   @tagged_with_posts = current_user.posts.tagged_with(@id).order(created_at: :desc)
+  sum_running_time(@tagged_with_posts)
 end
 
 
