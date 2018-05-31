33549f116b7c149abf0e84c3fb6e801e74163c37
MH4GF <rixi436@gmail.com>
2018-05-31 20:33:28 +0900

    メソッド名を@postsから@tagged_with_postsに変えた

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -7,7 +7,7 @@ end
 
 def show
   @id = ActsAsTaggableOn::Tag.find_by(id: params[:id])
-  @posts = User.find_by(id: current_user.id).posts.tagged_with(@id).order(created_at: :desc)
+  @tagged_with_posts = current_user.posts.tagged_with(@id).order(created_at: :desc)
 end
 
 
