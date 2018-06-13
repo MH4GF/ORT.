15f7e49e1dfdededdbe4ab47026a32f3600a6b72
MH4GF <rixi436@gmail.com>
2018-06-14 00:29:31 +0900

    callbackurl

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -256,7 +256,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET'], callback_url: "https://ort.herokuapp.com//users/auth/twitter/callback"
+  config.omniauth :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET'], callback_url: "https://ort.herokuapp.com/users/auth/twitter/callback"
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
