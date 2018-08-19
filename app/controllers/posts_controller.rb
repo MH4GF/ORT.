18e787d6cf5350205cecd20046d64edde72fa0d8
MH4GF <rixi436@gmail.com>
2018-08-19 13:57:05 +0900

    Tweet機能をモデルに委譲

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,62 +1,30 @@
 class PostsController < ApplicationController
 
-
   def new
-    @post = Post.new
+    @post               = Post.new
     @default_time       = current_user.default_time
     @allow_linked_tweet = current_user.allow_linked_tweet
   end
 
-  def tweet
-    client = Twitter::REST::Client.new do |config|
-      config.consumer_key         = ENV['TWITTER_CONSUMER_KEY']
-      config.consumer_secret      = ENV['TWITTER_CONSUMER_SECRET']
-      config.access_token         = current_user.token
-      config.access_token_secret  = current_user.secret
-    end
-    # Twitter投稿
-    client.update(tweet_contents)
-    flash[:notice] = "投稿とツイートが完了しました。5分休憩しましょう！"
-  end
-
-  # ツイート内容
-  def tweet_contents
-    return create_params[:content] +
-           "【" + create_params[:running_time] + "分】" +
-           "#インターネット勉強班 @ORT_pomodoro"
-  end
-
-
   def create
-    @post = Post.new(content:      create_params[:content],
-                     running_time: create_params[:running_time],
-                     tag_list:     create_params[:tag_list],
-                     user_id:      current_user.id)
-
+    @post = current_user.posts.new(create_params)
+    byebug
     if @post.save
       flash[:notice] = "投稿を作成しました。5分休憩しましょう！"
 
       # ツイートトグルが有効の場合ツイート
-      if params[:tweet_toggle] === "true"
-        tweet
+      if params[:tweet_toggle]
+        @post.tweet
+        flash[:notice] = "投稿とツイートが完了しました。5分休憩しましょう！"
       end
 
-      # タグがある場合はタグ一覧ページにリダイレクトし、ない場合はマイページへリダイレクト
-      if @post.tag_list != []
-        latest_post = Post.all.last
-        tag = ActsAsTaggableOn::Tagging.find_by(taggable_id: latest_post.id)
-        redirect_to("/tags/#{tag.tag_id}")
+      if @post.tags.present?
+        redirect_to tag_path(tags.first.id)
       else
-        redirect_to("/users/mypage")
+        redirect_to user_root_path
       end
-
     else
-
-      # 投稿ページにリダイレクトするならタイマーのモーダルは出さない
-      # FIXME なんだこれ
-      @display_none = "display_none"
-
-      render("posts/new")
+      render :new
 
     end
 
@@ -76,9 +44,9 @@ class PostsController < ApplicationController
     # 投稿が保存できたかどうかで条件分岐
     if @post.save
       flash[:notice] = "投稿を編集しました"
-      redirect_to("/users/mypage")
+      redirect_to user_root_path
     else
-      render("posts/edit")
+      render :edit
     end
   end
 
@@ -86,11 +54,11 @@ class PostsController < ApplicationController
     @post = Post.find_by(id: params[:id])
     @post.destroy
     flash[:notice] = "投稿を削除しました"
-    redirect_to("/users/mypage")
+    redirect_to user_root_path
   end
 
-private
-  # ストロングパラメーター
+  private
+
   def create_params
     params.require(:post).permit(:content, :running_time, :tag_list)
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,22 @@ class Post < ApplicationRecord
   belongs_to :user
   validates :content, {presence: true}
 
-
-  acts_as_taggable_on
   acts_as_taggable
+
+  def tweet
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key         = ENV['TWITTER_CONSUMER_KEY']
+      config.consumer_secret      = ENV['TWITTER_CONSUMER_SECRET']
+      config.access_token         = self.user.token
+      config.access_token_secret  = self.user.secret
+    end
+    # Twitter投稿
+    client.update(tweet_contents)
+  end
+
+  # ツイート内容
+  def tweet_contents
+    "#{content} 【#{running_time}分】#インターネット勉強班 @ORT_pomodoro"
+  end
+
 end
