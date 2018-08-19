505d2184230862411da8846619daea7d35c17fb1
MH4GF <rixi436@gmail.com>
2018-08-19 20:29:16 +0900

    application_controller周りリファクタリング

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,49 +1,21 @@
 class ApplicationController < ActionController::Base
-  # CSRF対策
   protect_from_forgery with: :exception
 
-  before_action :posts, :tags, :move_to_about
+  before_action :user_info, :move_to_about
 
-  def posts
-    if current_user === nil
-      return
-    end
+  def user_info
+    return unless user_signed_in?
 
-    @posts = current_user.posts.order(created_at: :desc)
-    sum_running_time(@posts)
-    @posts_hour = @sum / 60
-    @posts_min = @sum % 60
-  end
-
-  def tags
-    if current_user === nil
-      return
-    end
-
-    @all_tags = ActsAsTaggableOn::Tag.all
-  end
-
-  # 累計時間を計算
-  def sum_running_time(some_posts)
-    if current_user === nil
-      return
-    end
-
-    @sum = 0
-
-    # postsに入っている投稿の配列から、running_timeカラムに入っている時間を@sumに足していく
-    some_posts.each do |post|
-      @sum += post.running_time.to_i
-    end
-
+    @posts                = current_user.posts.order(created_at: :desc)
+    @total_running_time   = SumRunningTimeService.new(@posts).call
+    @having_tags          = @posts.tag_counts
   end
 
   # ログインしていない場合はaboutへリダイレクト
   def move_to_about
     unless user_signed_in?
       flash[:notice] = "ログインして、学習を始めよう！"
-      redirect_to controller: 'docs', action: 'about'
+      redirect_to root_path
     end
   end
-
 end
