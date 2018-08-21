ebcee02f22287c1f21f0d203e30b78edd3ea2663
MH4GF <rixi436@gmail.com>
2018-08-21 22:05:22 +0900

    累計時間がおかしかったので修正

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,7 @@
 class TagsController < ApplicationController
   def show
-    @tag                = ActsAsTaggableOn::Tag.find_by(id: params[:id])
-    @tagged_with_posts  = current_user.posts.tagged_with(@tag).order(created_at: :desc)
-    @total_running_time = SumRunningTimeService.new(@tagged_with_posts).call
+    @tag                     = ActsAsTaggableOn::Tag.find_by(id: params[:id])
+    @tagged_with_posts       = current_user.posts.tagged_with(@tag).order(created_at: :desc)
+    @tags_total_running_time = SumRunningTimeService.new(@tagged_with_posts).call
   end
 end
